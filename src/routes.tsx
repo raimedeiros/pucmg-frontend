e13ef47279ea82fd0932b17997f2aa7d1b1b7314
Rai Medeiros <raismedeiros@gmail.com>
2020-07-16 00:57:39 -0300

    Configuração das rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import {Route, BrowserRouter} from 'react-router-dom'
 
 import Login from './pages/Login'
+import Estoques from './pages/Estoques'
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Route component={Login} path="/" exact />
+            <Route component={Estoques} path="/estoques"/>
         </BrowserRouter>
     )
 }
