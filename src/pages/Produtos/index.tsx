d3efb699f2e3cf98e36c791746e1c1b51c0f8d35
Rai Medeiros <raismedeiros@gmail.com>
2020-08-02 23:40:19 -0300

    Informações sobre itens proximos ao vencimento

--- src/pages/Produtos/index.tsx
+++ src/pages/Produtos/index.tsx
@@ -14,24 +14,58 @@ interface Produto {
   expires:Date;
   formattedExpires:string;
   amount:number;
+  expireStatus: 'green' | 'yellow' | 'red'
+}
+interface AlertaProdutos{
+  vencidos:number,
+  proximoVencimento:number,
 }
 export default function Produtos({estoqueSelecionado}:ProdutoProps){
   
   const [produtos, setProdutos] = useState<Produto[]>([])
+  const [alertaProdutos, setAlertaProdutos] = useState<AlertaProdutos>({}as AlertaProdutos)
   
   useEffect(()=>{
     async function loadProducts(){
       const response = await api.get(`produtos?estoque=${estoqueSelecionado}`)
-      const formattedProducts = response.data.map((product:Produto)=>({
-        ...product,
-        formattedExpires: new Date(product.expires).toLocaleDateString(
-          'pt-br',
-        ),
-      }))
+      let vencidos = 0
+      let proximoVencimento = 0
+
+      const formattedProducts = response.data.map((product:Produto)=>{
+
+        const today = new Date().getTime()
+        const expiringDate = new Date(product.expires).getTime()
+
+        const daysToExpire = (expiringDate - today) / (1000*60*60*24)
+
+         let expireStatus= 'green' 
+
+        if(daysToExpire<5 && daysToExpire > 0){
+          expireStatus= 'yellow' 
+          proximoVencimento += 1
+        }
+        
+        if(daysToExpire < 0){
+          expireStatus= 'red' 
+          vencidos += 1
+        } 
+
+        const formattedExpires = new Date(product.expires).toLocaleDateString('pt-br')
+        return {
+          ...product,
+          formattedExpires,
+          expireStatus
+        }
+      })
+      setAlertaProdutos({
+        vencidos,
+        proximoVencimento
+      })
       setProdutos(formattedProducts)
     }
     loadProducts()
-  },[estoqueSelecionado])
+   },[estoqueSelecionado])
+
   return(
     <div id="page-produtos">
       <Row>
@@ -44,6 +78,14 @@ export default function Produtos({estoqueSelecionado}:ProdutoProps){
           <Link to={`produtos/create/${estoqueSelecionado}`}>
             <button className="button-roxo"><FiPlusCircle></FiPlusCircle>Adicionar produto</button>
           </Link>
+        </Col>
+      </Row>
+      <Row className="alerta-produtos"> 
+      <Col md={12}>
+          {(alertaProdutos.proximoVencimento>0)&&<p className="alerta-amarelo">{alertaProdutos.proximoVencimento} itens próximos ao vencimento</p>}
+          </Col>
+          <Col md={12}>
+          {(alertaProdutos.vencidos>0)&&<p className="alerta-vermelho">{alertaProdutos.vencidos} itens vencidos</p>}
         </Col>
       </Row>
       <Row>
@@ -62,7 +104,7 @@ export default function Produtos({estoqueSelecionado}:ProdutoProps){
                 produtos.map(produto=>(
                   <tr className="linha-produto" key={produto.id}>
                       <td className="cell-name">{produto.name}</td>
-                      <td className="cell-expires">{produto.formattedExpires}</td>
+                      <td className={`cell-expires ${produto.expireStatus}`}>{produto.formattedExpires}</td>
                       <td className="cell-amount">{produto.amount}</td>
                       <td className="cell-actions"><FiEdit></FiEdit><FiTrash2></FiTrash2></td>
                   </tr>
