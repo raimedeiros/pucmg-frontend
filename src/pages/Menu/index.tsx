956c77b8584f24a9e5ad1bb6da269bffb7985e45
Rai Medeiros <raismedeiros@gmail.com>
2020-08-07 14:07:45 -0300

    Correções de responsividade no menu

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,44 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FiHome, FiLogOut } from 'react-icons/fi';
+import { FiHome, FiLogOut, FiMenu } from 'react-icons/fi';
 
 import LogoMin from '../../assets/Logo-min.svg';
 import './menu.css';
 
 const Menu: React.FC = () => {
+  const [flagMenu, setFlagMenu] = useState<boolean>(false);
+
+  async function handleMenu(): Promise<void> {
+    if (!flagMenu) {
+      setFlagMenu(true);
+    } else {
+      setFlagMenu(false);
+    }
+  }
+
   return (
     <div id="page-menu">
-      <div className="menu-logo">
-        <img src={LogoMin} alt="Logo" />
-      </div>
-      <div className="breadcrumbs">
-        <FiHome />
-        <div>
-          Início
-          {' > '}
-          Estoque
+      <div
+        className={`responsive-menu ${
+          flagMenu ? 'mostrar-menu' : 'esconder-menu'
+        }`}
+      >
+        <div className="menu-logo">
+          <div className="logo-img">
+            <img src={LogoMin} alt="Logo" />
+          </div>
+          <div className="menu-trigger">
+            <button type="button" onClick={handleMenu}>
+              <FiMenu size={30} />
+            </button>
+          </div>
         </div>
-      </div>
-      <ul className="lista-menu">
-        <li>
-          <Link to="/estoques">Estoques</Link>
-        </li>
-        <li>
-          <Link to="/desperdicios">Desperdícios</Link>
-        </li>
-        <li>
-          <Link to="/fornecedores">Fornecedores</Link>
-        </li>
-        <li>
-          <Link to="/funcionarios">Funcionários</Link>
-        </li>
-        <li>
-          <Link to="/relatorios/desperdicios">Relatório</Link>
-        </li>
-      </ul>
-      <div className="link-sair">
-        <FiLogOut />
-        <Link to="/">Sair</Link>
+        <div className="lista-menu">
+          <ul>
+            <li>
+              <Link to="/estoques">Estoques</Link>
+            </li>
+            <li>
+              <Link to="/desperdicios">Desperdícios</Link>
+            </li>
+            <li>
+              <Link to="/fornecedores">Fornecedores</Link>
+            </li>
+            <li>
+              <Link to="/funcionarios">Funcionários</Link>
+            </li>
+            <li>
+              <Link to="/relatorios/desperdicios">Relatório</Link>
+            </li>
+          </ul>
+          <div className="link-sair">
+            <FiLogOut />
+            <Link to="/">Sair</Link>
+          </div>
+        </div>
       </div>
     </div>
   );
