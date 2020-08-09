8c7c37636c141d1c55ff4395356944df777dc801
Rai Medeiros <raismedeiros@gmail.com>
2020-08-09 19:20:56 -0300

    Implementação da rota autenticada

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,16 +1,53 @@
-import React, { FormEvent } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import './login.css';
+import React, { FormEvent, useState, ChangeEvent } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import { FiLogIn, FiPlusCircle, FiUser, FiKey } from 'react-icons/fi';
 import { Container, Row, Col } from 'react-grid-system';
 
+import { useAuth } from '../../context/auth.js';
+import api from '../../services/api';
+import './login.css';
+
 const Login: React.FC = () => {
-  const history = useHistory();
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const { setAuthTokens } = useAuth();
+
+  async function handleInputChange(
+    event: ChangeEvent<HTMLInputElement>,
+  ): Promise<void> {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  }
 
   async function handleSubmit(event: FormEvent): Promise<void> {
     event.preventDefault();
-    history.push('/estoques');
+
+    api
+      .post('sessions', formData)
+      .then(response => {
+        if (response.status === 200) {
+          setAuthTokens(response.data);
+          setLoggedIn(true);
+        } else {
+          setIsError(true);
+          setLoggedIn(false);
+        }
+      })
+      .catch(e => {
+        setIsError(true);
+        setLoggedIn(false);
+      });
   }
+
+  if (isLoggedIn) {
+    return <Redirect to="/estoques" />;
+  }
+
   return (
     <Container fluid id="page-login">
       <Row className="content">
@@ -20,10 +57,15 @@ const Login: React.FC = () => {
         <Col xs={12} md={4} offset={{ md: 4 }}>
           <form onSubmit={handleSubmit}>
             <div className="field">
-              <label htmlFor="usuario">
-                Usuário
+              <label htmlFor="email">
+                E-mail
                 <div className="input-box">
-                  <input type="text" name="usuario" id="usuario" />
+                  <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    onChange={handleInputChange}
+                  />
                   <span className="dcr-input">
                     <FiUser />
                   </span>
@@ -31,16 +73,28 @@ const Login: React.FC = () => {
               </label>
             </div>
             <div className="field">
-              <label htmlFor="senha">
+              <label htmlFor="password">
                 Senha
                 <div className="input-box">
-                  <input type="password" name="senha" id="senha" />
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    onChange={handleInputChange}
+                  />
                   <span className="dcr-input">
                     <FiKey />
                   </span>
                 </div>
               </label>
             </div>
+
+            {isError && (
+              <div className="info-login-error">
+                <span>O usuário ou senha estão incorretos.</span>
+              </div>
+            )}
+
             <div className="submit-button">
               <button type="submit" className="button-roxo">
                 <FiLogIn />
